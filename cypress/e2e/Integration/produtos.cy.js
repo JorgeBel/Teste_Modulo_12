97cf7625e370a108fca49da7f396719972670b66
JorgeBel <jorgeluizbelodelima@gmail.com>
2023-11-01 22:35:15 -0300

    alteracoes no teste automatizado

---
+++ cypress/e2e/Integration/produtos.cy.js
@@ -0,0 +1,35 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade páginas de produtos', () => {
+
+    beforeEach(() => {
+        cy.visit("http://lojaebac.ebaconline.art.br/produtos/")
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('[class="product-block grid"]')
+            //.first()
+            //.last()
+            //.eq(3)
+            .contains("Caesar Warm-Up Pant")
+            .click()
+    });
+
+    it.only('deve adicionar um produto ao carrinho', () => {
+        var quantidade = 3
+
+        cy.get('[class="product-block grid"]')
+            .contains("Beaumont Summit Kit").click()
+        cy.get('.button-variable-item-S').click()
+        cy.get('.button-variable-item-Orange').click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+
+
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
+        cy.get('.woocommerce-message').should('contain', quantidade+' × “Beaumont Summit Kit” foram adicionados no seu carrinho.')
+
+    });
+
+});
+
