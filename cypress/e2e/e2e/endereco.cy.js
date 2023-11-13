14fb633c8f8d51036d112b26cc24a3c438da71fc
JorgeBel <jorgeluizbelodelima@gmail.com>
2023-11-13 01:36:56 -0300

    adicionando relatório

--- cypress/e2e/e2e/endereco.cy.js
+++ cypress/e2e/e2e/endereco.cy.js
@@ -1,4 +1,6 @@
 /// <reference types="cypress"/>
+import EnderecoPage from "./endereco.page"
+import dadosEndereco from "./endereco.json"
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
 
@@ -11,6 +13,25 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     });
 
     it('Deve fazer cadastro de faturamento com sucesso', () => {
-        
+        EnderecoPage.editarEnderecoFaturamento("Caio", "Jorge", "Ebac", "Brasil", "Rua Capita", "300", "Rita", "São Paulo", "91150-031", "000000000000", "caioebac@gmail.com")
+        cy.get('.woocommerce-message').should("contain" , "Endereço alterado com sucesso")
     });
+
+    
+    it('Deve fazer cadastro de faturamento com sucesso - Usando arquivo de dados', () => {
+        EnderecoPage.editarEnderecoFaturamento
+        (dadosEndereco[1].nome,
+        dadosEndereco[1].sobrenome,
+        dadosEndereco[1].empresa,
+        dadosEndereco[1].pais,
+        dadosEndereco[1].endereco,
+        dadosEndereco[1].cidade,
+        dadosEndereco[1].estado,
+        dadosEndereco[1].cep,
+        dadosEndereco[1].telefone,
+        dadosEndereco[1].email
+        )
+        cy.get('.woocommerce-message').should("contain" , "Endereço alterado com sucesso")
+    });
+
 });
